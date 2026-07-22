4c58d7fb817c21f3c6ebf9ec28193793a4277416
mahi2025 <mahletmasresha08@gmail.com>
2026-07-22 11:44:26 +0300

    chore: health route checked

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
+import { validationSchema } from './config/validation.schema';
+import { HealthController } from './health/health.controller';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports:[
+    ConfigModule.forRoot({
+      isGlobal:true,
+      load:[config],
+      validationSchema,
+    }),
+  ],
+  controllers: [HealthController],
 })
+
 export class AppModule {}
