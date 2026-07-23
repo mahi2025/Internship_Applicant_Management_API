8cba8d78231b4ec84913e3ed3d8a570931ec7049
mahi2025 <mahletmasresha08@gmail.com>
2026-07-23 09:35:54 +0300

    feat(auth): authentication module created

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { Public } from './decorators/public.decorator';
+import { CurrentUser } from './decorators/current-user.decorator';
+
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @Get('me')
+  me(@CurrentUser() user: {
+     id: string; 
+     email: string 
+    }) {
+    return user;
+  }
+}
