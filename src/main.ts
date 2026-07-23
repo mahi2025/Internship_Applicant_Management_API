328e834bdb119136aff29c490c3cea874b736798
mahi2025 <mahletmasresha08@gmail.com>
2026-07-23 14:49:14 +0300

    feat(validation): configured global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,11 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.setGlobalPrefix('api', {
+    exclude: [],
+  });
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('port') ?? 3000;
