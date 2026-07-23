49bad3712bcf068178acdb30e4b764cbd957477f
mahi2025 <mahletmasresha08@gmail.com>
2026-07-23 09:37:10 +0300

    feat(auth): JWT auth implemented with passport strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+interface JwtPayload {
+  sub: string;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: 
+      ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+
+      secretOrKey: configService.getOrThrow<string>('config.jwt.secret'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+
+    const admin = await this.authService.validateAdminById(payload.sub);
+    return { id: admin.id, email: admin.email };
+  }
+}
