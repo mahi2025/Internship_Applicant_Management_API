72a83636504bfa24dc28251a28e14c284a81db90
mahi2025 <mahletmasresha08@gmail.com>
2026-07-23 10:03:26 +0300

    modified files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/config';
 import { validationSchema } from './config/validation.schema';
 import { HealthController } from './health/health.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 
 @Module({
-  imports:[
+  imports: [
     ConfigModule.forRoot({
-      isGlobal:true,
-      load:[config],
+      isGlobal: true,
+      load: [config],
       validationSchema,
     }),
+    PrismaModule,
+    AuthModule,
   ],
   controllers: [HealthController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 
 export class AppModule {}
